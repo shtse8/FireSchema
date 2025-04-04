1f3ae2e9eb21b5b36658354040639df4760c2317
shtse8 <shtse8@gmail.com>
2025-04-04 07:19:20 +0100

    refactor: move runtime packages to separate repos using submodules

--- packages/fireschema_dart_runtime/lib/src/types.dart
+++
@@ -1,5 +0,0 @@
-/// Interface for classes that can be serialized to a JSON-like map.
-abstract class ToJsonSerializable {
-  /// Converts this instance to a Map suitable for Firestore operations.
-  Map<String, Object?> toJson();
-}
